31bebc73dd5483c5c5a2831c74ce021da4a7fb59
francgc2-hub <francgc2@gmail.com>
2026-03-10 23:23:51 -0600

    Cambios

--- Denavit_Mitsubishi RV-12CRL-D.py
+++ Denavit_Mitsubishi RV-12CRL-D.py
@@ -36,32 +36,32 @@ theta_1, theta_2, theta_3, theta_4, theta_5, theta_6 = sp.symbols(
 T01 = T.subs({d: 0.450, a: 0.130, alpha: -sp.pi/2})
 T01 = T01.subs({theta: theta_1})
 
-#sp.pprint(T01)
+sp.pprint(T01)
 
 T12 = T.subs({d: 0, a: 0.600, alpha: 0})  # theta2 - sp.pi/2 (si aplica, ponlo en theta)
 T12 = T12.subs({theta: theta_2})
 
-#sp.pprint(T12)
+sp.pprint(T12)
 
 T23 = T.subs({d: 0, a: 0.150, alpha: -sp.pi/2})
 T23 = T23.subs({theta: theta_3})
 
-#sp.pprint(T23)
+sp.pprint(T23)
 
 T34 = T.subs({d: 0.760, a: 0, alpha: sp.pi/2})  
 T34 = T34.subs({theta: theta_4})
 
-#sp.pprint(T34)
+sp.pprint(T34)
 
 T45 = T.subs({d: 0, a: 0, alpha: -sp.pi/2}) # theta5 - sp.pi/2 (si aplica, ponlo en theta)
 T45 = T45.subs({theta: theta_5})
 
-#sp.pprint(T45)  
+sp.pprint(T45)  
 
 T56 = T.subs({d: 0.100, a: 0, alpha: 0}) # theta6 - sp.pi/2 (si aplica, ponlo en theta)
 T56 = T56.subs({theta: theta_6})    
 
-#sp.pprint(T56)
+sp.pprint(T56)
 
 T06 = T01 @ T12 @ T23 @ T34 @ T45 @ T56
 T06_s = T06.applyfunc(sp.simplify)
